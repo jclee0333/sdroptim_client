dca7e89f229042eac1122112765356ed54e9db29
jclee0333 <jclee0333@gmail.com>
2020-11-23 21:28:54 +0900

    Add files via upload

--- sdroptim_client/__init__.py
+++ sdroptim_client/__init__.py
@@ -3,3 +3,14 @@ from sdroptim_client.from_json_to_scripts import FullscriptsGenerator, get_defau
 from sdroptim_client.hpo_job_submit import SubmitHPOjob, get_params, set_params
 from sdroptim_client.hpo_job_submit import create_hpojob, load_hpojob
 from sdroptim_client.visualization import get_default_chart_html
+def get_portal_address(configure_json='run_conf.json'):
+    import os, json
+    with open(configure_json, 'r') as f:
+        res = json.load(f)['gid']
+    GID = os.getenv('NB_GID')
+    for k,v in res.items():
+        if GID == k:
+            return v
+        elif GID == 'default':
+            default_v = v
+    return v
