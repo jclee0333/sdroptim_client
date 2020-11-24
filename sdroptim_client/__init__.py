175b8b143ce6c02cf0792762c9518c163b13e544
jclee0333 <jclee0333@gmail.com>
2020-11-24 14:31:49 +0900

    Update __init__.py

--- sdroptim_client/__init__.py
+++ sdroptim_client/__init__.py
@@ -4,7 +4,7 @@ from sdroptim_client.hpo_job_submit import SubmitHPOjob, get_params, set_params
 from sdroptim_client.hpo_job_submit import create_hpojob, load_hpojob
 from sdroptim_client.visualization import get_default_chart_html
 def get_portal_address(configure_json='run_conf.json'):
-    import os, json, inspect
+    import os, json, inspect, sys
     with open(os.path.dirname(inspect.getfile(sys.modules['sdroptim_client']))+os.sep+configure_json, 'r') as f:
         res = json.load(f)['gid']
     GID = os.getenv('NB_GID')
