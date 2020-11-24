59f2857457dfb2e90c03c70c1258d87a771df6e1
jclee0333 <jclee0333@gmail.com>
2020-11-24 14:22:11 +0900

    Add files via upload

--- sdroptim_client/__init__.py
+++ sdroptim_client/__init__.py
@@ -4,8 +4,8 @@ from sdroptim_client.hpo_job_submit import SubmitHPOjob, get_params, set_params
 from sdroptim_client.hpo_job_submit import create_hpojob, load_hpojob
 from sdroptim_client.visualization import get_default_chart_html
 def get_portal_address(configure_json='run_conf.json'):
-    import os, json
-    with open(configure_json, 'r') as f:
+    import os, json, inspect
+    with open(os.path.dirname(inspect.getfile(sdroptim_client))+os.sep+configure_json, 'r') as f:
         res = json.load(f)['gid']
     GID = os.getenv('NB_GID')
     for k,v in res.items():
